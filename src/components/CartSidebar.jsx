19c23c449231ecfd4ee817a1d28012d426b71a1c
ThangaPreethi-03 <thangapreethi85@gmail.com>
2025-12-15 21:54:50 +0530

    Update CartSidebar.jsx

--- src/components/CartSidebar.jsx
+++ src/components/CartSidebar.jsx
@@ -1,7 +1,8 @@
 // src/components/CartSidebar.jsx
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../App";
+
+import { useAuth } from "../context/AuthContext";
 
 export default function CartSidebar({
   open,
