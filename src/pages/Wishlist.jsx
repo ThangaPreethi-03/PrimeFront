80f35b4688f9453025e05a504b398ee06a65cb1e
ThangaPreethi-03 <thangapreethi85@gmail.com>
2025-12-15 21:59:58 +0530

    Update Wishlist.jsx

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
-import { useAuth } from "../App";
+
+import { useAuth } from "../context/AuthContext";
 
 export default function Wishlist() {
   const navigate = useNavigate();
